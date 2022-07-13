d52519b70fd624a1c7e01aee4a017a041fb3fc71
Finn <34718806+FinnDore@users.noreply.github.com>
2022-07-13 11:38:46 +0100

    chore: added tailwind and adjusted the landing page

--- apps/profile/pages/index.tsx
+++ apps/profile/pages/index.tsx
@@ -1,9 +1,9 @@
-import styles from './index.module.scss';
 export default function index() {
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>FINN DORE</h1>
-            <h1 className={styles['sub-title']}>🚧 Coming Soon 🚧</h1>
+        <div className="bg-black h-full grid place-items-center">
+            <h1 className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-teal-400 to-purple-800">
+                FINN DORE
+            </h1>
         </div>
     );
 }
