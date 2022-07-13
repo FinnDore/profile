f2289d67c9b5845f917d96acf69473d846e37b52
Finn <34718806+FinnDore@users.noreply.github.com>
2022-07-13 22:39:48 +0100

    chore: added a moon

--- apps/profile/pages/index.tsx
+++ apps/profile/pages/index.tsx
@@ -1,9 +1,19 @@
+import styles from './index.module.scss';
+
 export default function index() {
     return (
-        <div className="grid h-full place-items-center bg-black">
-            <h1 className="bg-gradient-to-br from-red-500 to-pink-500 bg-clip-text text-5xl font-extrabold text-transparent sm:text-6xl md:text-8xl">
-                FINN DORE
-            </h1>
+        <div className="relative h-full overflow-hidden bg-black">
+            <img
+                className={`${styles.moon} absolute`}
+                src="/moon.svg"
+                alt="big grey ball in the sky"
+            />
+
+            <div className="grid h-full place-items-center">
+                <h1 className="left-50 top-90 absolute bg-gradient-to-br from-stone-900 to-zinc-50 bg-clip-text text-5xl font-extrabold text-transparent sm:text-6xl md:text-8xl">
+                    FINN DORE
+                </h1>
+            </div>
         </div>
     );
 }
