50e4bb46ddfdef3dcaaf70e15ecd415293da78ef
Finn <34718806+FinnDore@users.noreply.github.com>
2022-07-13 12:04:42 +0100

    chore: added tailwind prettier

--- apps/profile/pages/index.tsx
+++ apps/profile/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function index() {
     return (
-        <div className="bg-black h-full grid place-items-center">
-            <h1 className="font-extrabold text-transparent text-6xl md:text-8xl bg-clip-text bg-gradient-to-r from-teal-400 to-purple-800">
+        <div className="grid h-full place-items-center bg-black">
+            <h1 className="bg-gradient-to-r from-teal-400 to-purple-800 bg-clip-text text-5xl font-extrabold text-transparent sm:text-6xl md:text-8xl">
                 FINN DORE
             </h1>
         </div>
