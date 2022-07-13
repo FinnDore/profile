914352a17301d1b1e6321649f923e113724f256b
Finn <34718806+FinnDore@users.noreply.github.com>
2022-07-13 12:14:52 +0100

    chore: changed gradient color

--- apps/profile/pages/index.tsx
+++ apps/profile/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function index() {
     return (
         <div className="grid h-full place-items-center bg-black">
-            <h1 className="bg-gradient-to-r from-teal-400 to-purple-800 bg-clip-text text-5xl font-extrabold text-transparent sm:text-6xl md:text-8xl">
+            <h1 className="bg-gradient-to-br from-red-500 to-pink-500 bg-clip-text text-5xl font-extrabold text-transparent sm:text-6xl md:text-8xl">
                 FINN DORE
             </h1>
         </div>
