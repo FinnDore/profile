68fee060f18767afa645eea4a87f0c82e2004b6d
Finn <34718806+FinnDore@users.noreply.github.com>
2022-07-13 11:46:07 +0100

    fix: fixed text size on small devices

--- apps/profile/pages/index.tsx
+++ apps/profile/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function index() {
     return (
         <div className="bg-black h-full grid place-items-center">
-            <h1 className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-teal-400 to-purple-800">
+            <h1 className="font-extrabold text-transparent text-6xl md:text-8xl bg-clip-text bg-gradient-to-r from-teal-400 to-purple-800">
                 FINN DORE
             </h1>
         </div>
