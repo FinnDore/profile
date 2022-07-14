19ee4099c8497f74f20fb8d137ad146c4d88cb9e
Finn <34718806+FinnDore@users.noreply.github.com>
2022-07-14 18:10:49 +0100

    chore: adjusted the parolax speed

--- apps/profile/pages/index.tsx
+++ apps/profile/pages/index.tsx
@@ -4,22 +4,17 @@ import { useRef } from 'react';
 import styles from './index.module.scss';
 
 function Star() {
-    return <div className={`${styles.star} absolute bg-amber-200`}></div>;
+    return <div className={`${styles.star} r-50 absolute bg-amber-200`}></div>;
 }
 
 function Page() {
     const parallaxRef = useRef();
     return (
         <Parallax pages={2} ref={parallaxRef}>
-            <ParallaxLayer
-                offset={0}
-                speed={3.5}
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}
-            >
+            <ParallaxLayer offset={0} speed={0.5}>
+                <Star />
+            </ParallaxLayer>
+            <ParallaxLayer offset={0} speed={3.5}>
                 <img
                     className={`${styles.moon} absolute`}
                     src="/moon.svg"
@@ -28,11 +23,10 @@ function Page() {
             </ParallaxLayer>
             <ParallaxLayer
                 offset={0}
-                speed={2.5}
+                speed={2.75}
                 style={{
                     display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center'
+                    justifyContent: 'center'
                 }}
             >
                 <div className="grid place-items-center">
@@ -41,13 +35,30 @@ function Page() {
                     </h1>
                 </div>
             </ParallaxLayer>
+
+            <ParallaxLayer offset={1} speed={5.2}>
+                <div className="text-white">
+                    <h1 className="bold">Some header</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </p>
+                </div>
+            </ParallaxLayer>
         </Parallax>
     );
 }
 
 export default function index() {
     return (
-        <div className="relative flex h-[100vh] bg-black">
+        <div className="relative h-[100vh] bg-black">
             <Star />
             <Page />
         </div>
