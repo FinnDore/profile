6aac5cdc51f3c918b71bc05a9c54cef9dbd73697
Finn <34718806+FinnDore@users.noreply.github.com>
2022-07-14 16:52:06 +0100

    chore: added a basic paraolax

--- apps/profile/pages/index.tsx
+++ apps/profile/pages/index.tsx
@@ -1,19 +1,55 @@
+/* eslint-disable @next/next/no-img-element */
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { useRef } from 'react';
 import styles from './index.module.scss';
+
+function Star() {
+    return <div className={`${styles.star} absolute bg-amber-200`}></div>;
+}
+
+function Page() {
+    const parallaxRef = useRef();
+    return (
+        <Parallax pages={2} ref={parallaxRef}>
+            <ParallaxLayer
+                offset={0}
+                speed={3.5}
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <img
+                    className={`${styles.moon} absolute`}
+                    src="/moon.svg"
+                    alt="big grey ball in the sky"
+                />
+            </ParallaxLayer>
+            <ParallaxLayer
+                offset={0}
+                speed={2.5}
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+            >
+                <div className="grid place-items-center">
+                    <h1 className="left-50 top-90 absolute bg-gradient-to-br from-stone-900 to-zinc-50 bg-clip-text text-5xl font-extrabold text-transparent sm:text-6xl md:text-8xl">
+                        FINN DORE
+                    </h1>
+                </div>
+            </ParallaxLayer>
+        </Parallax>
+    );
+}
 
 export default function index() {
     return (
-        <div className="relative h-full overflow-hidden bg-black">
-            <img
-                className={`${styles.moon} absolute`}
-                src="/moon.svg"
-                alt="big grey ball in the sky"
-            />
-
-            <div className="grid h-full place-items-center">
-                <h1 className="left-50 top-90 absolute bg-gradient-to-br from-stone-900 to-zinc-50 bg-clip-text text-5xl font-extrabold text-transparent sm:text-6xl md:text-8xl">
-                    FINN DORE
-                </h1>
-            </div>
+        <div className="relative flex h-[100vh] bg-black">
+            <Star />
+            <Page />
         </div>
     );
 }
