393fae70aed463e77d3a5cea63f1e4b85212b06d
Finn <34718806+FinnDore@users.noreply.github.com>
2022-07-15 15:34:54 +0100

    feat: added an about me section (#3)

--- apps/profile/pages/index.tsx
+++ apps/profile/pages/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import { useRef } from 'react';
+import AboutMe from '../ui/about-me';
 import { StarField } from '../ui/star-field';
 import styles from './index.module.scss';
 
@@ -12,27 +13,32 @@ function Page() {
                 <StarField />
             </ParallaxLayer>
             <ParallaxLayer offset={0} speed={3.5}>
-                <img
-                    className={`${styles.moon} absolute`}
-                    src="/moon.svg"
-                    alt="big grey ball in the sky"
-                />
+                <div className="grid place-items-center">
+                    <img
+                        className={`${styles.moon} absolute z-[75] w-[150%] max-w-[1512px]`}
+                        src="/moon.svg"
+                        alt="big grey ball in the sky"
+                    />
+                </div>
             </ParallaxLayer>
             <ParallaxLayer
                 offset={0}
                 speed={2.75}
-                className="flex "
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center'
-                }}
+                className="flex justify-center"
             >
-                <div className="grid place-items-center">
+                <div className="mb-[10rem] grid place-items-center">
                     <h1 className="left-50 top-90 absolute bg-gradient-to-br from-stone-900 to-zinc-50 bg-clip-text text-5xl font-extrabold text-transparent sm:text-6xl md:text-8xl">
                         FINN DORE
                     </h1>
                 </div>
             </ParallaxLayer>
+
+            <ParallaxLayer offset={0.99} speed={2.75} className="z-[100]">
+                <div className="grid place-items-center">
+                    <AboutMe />
+                </div>
+            </ParallaxLayer>
+            <ParallaxLayer factor={20}></ParallaxLayer>
         </Parallax>
     );
 }
---
+++ apps/profile/ui/about-me.tsx
@@ -0,0 +1,34 @@
+export default function AboutMe() {
+    return (
+        <div className="absolute top-[15vh] z-[9999] px-10 text-zinc-300 md:top-[25vh]  lg:top-[40vh] lg:px-28">
+            <h1 className="text-base text-2xl font-extrabold text-zinc-400 md:text-4xl lg:text-6xl">
+                About Me
+            </h1>
+            <p className="py-2 text-sm drop-shadow-lg md:text-lg lg:py-5">
+                Frontend software engineer from the{' '}
+                <span className="text-cyan-500" title="🌧">
+                    UK
+                </span>{' '}
+                who likes to build pretty things.
+            </p>
+            <div className="flex">
+                <a
+                    className="px-2 pl-0 text-sm text-zinc-400 hover:text-zinc-200"
+                    href="mailto:finn@finndore.dev"
+                    rel="noreferrer"
+                    target="_blank"
+                >
+                    finn@finndore.dev
+                </a>
+                <a
+                    href="https://github.com/FinnDore"
+                    rel="noreferrer"
+                    target="_blank"
+                    className="px-2 text-sm text-zinc-400 hover:text-zinc-200"
+                >
+                    My GitHub
+                </a>
+            </div>
+        </div>
+    );
+}
