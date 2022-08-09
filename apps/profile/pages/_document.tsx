41a0cf9bbfb8144f638b78e7ed49899417a9a627
Finn <34718806+FinnDore@users.noreply.github.com>
2022-08-09 18:26:09 +0100

    feat: added umami (#6)

--- apps/profile/pages/_document.tsx
+++ apps/profile/pages/_document.tsx
@@ -4,11 +4,13 @@ export default function document() {
     return (
         <Html>
             <Head>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-                    rel="stylesheet"
-                />
-                <link rel="shortcut icon" href="/favicon.ico" />
+            <script async defer data-website-id="0acffcd3-e206-4840-bfb2-68c89da2e36e" src="https://umami.finndore.dev/umami.js"></script>
+
+            <link
+                href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+                rel="stylesheet"
+            />
+            <link rel="shortcut icon" href="/favicon.ico" />
             </Head>
             <meta property="og:title" content="Finn Dore" />
             <meta property="og:type" content="website" />
