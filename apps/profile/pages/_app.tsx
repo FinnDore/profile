07f61e3176a32913782045f4367bf1ba3193aef6
Finn <34718806+FinnDore@users.noreply.github.com>
2022-12-23 16:45:02 +0000

    cleanout

--- apps/profile/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import { AppProps } from 'next/app';
-import '../styles/reset.scss';
-import '../styles/styles.scss';
-
-export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
-}
