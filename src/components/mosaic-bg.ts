876a397240a137f380e0f20d0fa2a687bae98fcf
Finn <34718806+FinnDore@users.noreply.github.com>
2022-12-29 13:46:51 +0000

    "Changes"

---
+++ src/components/mosaic-bg.ts
@@ -0,0 +1,217 @@
+import { shaderMaterial } from '@react-three/drei';
+import type { ReactThreeFiber } from '@react-three/fiber';
+import { extend } from '@react-three/fiber';
+import type * as THREE from 'three';
+import { Vector2, Vector3 } from 'three';
+
+export type MosaicProps = {
+    resolution: Vector2;
+    zoom: number;
+    xy: Vector3;
+    time: number;
+    uTexture: THREE.Texture;
+};
+
+const MosaicMaterial = shaderMaterial(
+    {
+        time: 0,
+        resolution: new Vector2(1, 1),
+        zoom: 1,
+        xy: new Vector3(1, 1, 1)
+    },
+    `
+
+    #ifdef GL_ES
+    precision mediump float;
+    #endif
+    
+
+    varying vec3 Position;
+    varying vec2 vUv;   
+
+
+    void main() {
+        vUv = uv;
+        gl_Position =  projectionMatrix * modelViewMatrix * vec4(position, 1.0);  
+    }
+    `,
+    `
+
+    #ifdef GL_ES
+    precision mediump float;
+    #endif
+
+    uniform vec2 resolution;
+    uniform vec2 u_mouse;
+    uniform float time;
+    uniform sampler2D uTexture;
+
+    #define PI (3.14159265359)
+    #define TWO_PI (6.28318530718)
+
+    varying vec2 vUv;
+
+    float blendColorDodge(float base, float blend) {
+        return (blend==1.0)?blend:min(base/(1.0-blend),1.0);
+    }
+
+    vec3 blendColorDodge(vec3 base, vec3 blend) {
+        return vec3(blendColorDodge(base.r,blend.r),blendColorDodge(base.g,blend.g),blendColorDodge(base.b,blend.b));
+    }
+
+    vec3 blendColorDodge(vec3 base, vec3 blend, float opacity) {
+      return (blendColorDodge(base, blend) * opacity + base * (1.0 - opacity));
+    }
+
+    vec3 rgb(int r, int g, int b) {
+        return vec3(r, g, b) / 255.0;
+    }
+
+    mat2 Rotate(float angle) {
+        float s = sin(angle);
+        float c = cos(angle);
+
+        return mat2(c, -s, s, c);
+    }
+
+    float Shape(vec2 st, vec2 p, float size, float sides, float blur) {
+        vec2 pos = vec2(p) - st;
+        float a = atan(pos.x, pos.y) + PI;
+        float r = TWO_PI / sides;
+        float d = cos(floor(0.5 + a / r) * r - a) * length(pos);
+
+        float color = smoothstep(size + blur, size - 0.0, d);
+
+        return color;
+    }
+
+    float hollowTriangle(vec2 uv, vec2 position, float size, float blur) {
+        float sides = 3.0;
+
+        return Shape(uv, position, size, sides, blur) -
+        Shape(uv, position, size - 0.02, sides, blur);
+    }
+
+    float ring(vec2 p, float radius, float width) {
+        return abs(length(p) - radius * 0.5) - width;
+    }
+
+    float smoothedge(float v) {
+        return smoothstep(0.0, 1.0 / resolution.x, v);
+    }
+
+    vec3 Tiles(vec2 uv) {
+        vec3 result = vec3(0.0);
+        float zoom =  16.;
+        vec2 gv = fract(uv * zoom);
+
+        // Get a unique identifier for each tile
+        vec2 id = floor(uv * zoom);
+
+    
+        for (float y = -1.0; y <= 1.0; y++) {
+            for (float x = -1.0; x <= 1.0; x++) {
+                vec2 tileOffset = vec2(x, y);
+                vec2 triangleID = id + tileOffset;
+                vec2 tileShift = vec2(mod(triangleID.y, 2.0) * 0.5, 0.0);
+
+                if (
+                    hollowTriangle(
+                        gv - tileOffset - tileShift,
+                        vec2(0.5),
+                        0.19,
+                        0.001
+                    ) ==
+                    1.0
+                ) {
+                    result += vec3(0.51, 0.51, 0.51);
+                }
+
+                // Create a new set of UVs named  and rotate them around their center
+                vec2 st = (gv - 0.5) * Rotate(PI) + 0.5;
+
+                // Offset the new UVs by half of the width plus
+                // an arbitrary value for the Y axis.
+                st -= vec2(0.5, 0.37);
+
+                if (
+                    hollowTriangle(
+                        st - tileOffset + tileShift,
+                        vec2(0.5),
+                        0.254,
+                        0.001
+                    ) ==
+                    1.0
+                ) {
+                    result += vec3(0.069, 0.069, 0.069);
+                }
+
+                if (
+                    hollowTriangle(
+                        st - tileOffset + tileShift,
+                        vec2(0.5),
+                        0.142,
+                        0.001
+                    ) ==
+                    1.0
+                ) {
+                    result += vec3(0.8, 0.8, 0.8);
+                }
+
+            }
+        }
+
+        return result;
+    }
+
+    void main() {
+        vec3 color = vec3(0.0);
+        vec2 st =  gl_FragCoord.xy / resolution.x / 150.0;
+        st -= vec2(0.5);
+        st = Rotate( 2.25 * PI) * st;
+        st += vec2(0.5);
+        vec2 uv = st * 2.0 - 1.0;
+        uv.x *= resolution.x / resolution.y;
+
+        color += Tiles(uv);
+
+        vec3 col = vec3(uv, sin(time));
+        float speed = 0.0405;
+        vec2 center = vec2(1.5, 0.5);
+        float x = center.x - st.x;
+        float y = (center.y - st.y) * resolution.x / resolution.y;
+
+        float r = -(x * x + y * y);
+        //float r = -sqrt(x*x + y*y);
+        float ouuterZz = 0.1 + 0.5 * sin((r + time * speed) / 0.117);
+        float innerZ = 1.0 + 0.5 * sin((r + time * speed) / 0.053);
+        bool isOuterTrigangle = color.r == 0.069;
+        if (isOuterTrigangle && ouuterZz > 0.1) {
+            color.rgb = vec3(0.4) * ouuterZz;
+        } else if (isOuterTrigangle) {
+            color.rgb = vec3(0.0);
+        } else {
+            color *= innerZ;
+        }
+
+
+        vec3 bgColor = texture2D(uTexture, vUv).rgb;
+
+        gl_FragColor = vec4(bgColor, 1.); 
+        // gl_FragColor = vec4(color, 1.);
+        // gl_FragColor = vec4(blendColorDodge(color, bgColor, 0.5), 1.0)); 
+        // gl_FragColor = vec4(vec3(ouuterZz), 1.0);
+    }
+  `
+);
+
+extend({ MosaicMaterial });
+
+declare module '@react-three/fiber' {
+    interface ThreeElements {
+        mosaicMaterial: ReactThreeFiber.MaterialNode<
+            THREE.ShaderMaterial & MosaicProps,
+            MosaicProps
+        >;
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,34 @@
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber';
 import { type NextPage } from 'next';
 import { useEffect, useRef } from 'react';
-import { Vector2, Vector3 } from 'three';
+import THREE, { TextureLoader, Vector2, Vector3 } from 'three';
 import '../components/background-material';
-import type { BackgroundMaterialProps } from '../components/background-material';
+import '../components/mosaic-bg';
 
+import { Stats } from '@react-three/drei';
+import { MosaicProps } from '../components/mosaic-bg';
 const dumbby = new Vector3(1, 1, 1);
+
 function ShaderPlane() {
-    const ref = useRef<(THREE.ShaderMaterial & BackgroundMaterialProps) | null>(
-        null
-    );
+    const ref = useRef<(THREE.ShaderMaterial & MosaicProps) | null>(null);
 
     const { width, height } = useThree(state => state.viewport);
-    const zoomValue = useRef(256.0);
+    const zoomValue = useRef(1);
 
     const xyValue = useRef(new Vector3(1, 1, 1));
 
     useEffect(() => {
         const handleScroll = (e: WheelEvent) => {
+            return;
             if (ref.current) {
                 zoomValue.current =
                     e.deltaY < 0
                         ? zoomValue.current + 10
                         : zoomValue.current - 10;
             }
-            e.preventDefault();
         };
         const handleMouseMove = (e: MouseEvent) => {
             if (xyValue.current) {
-                xyValue.current.add(dumbby);
             }
         };
         window.addEventListener('mousemove', handleMouseMove);
@@ -39,7 +39,11 @@ function ShaderPlane() {
         };
     }, []);
 
-    const res = new Vector2(width, height);
+    const res = new Vector2(width, width);
+
+    const [image] = useLoader(TextureLoader, [
+        'https://images.unsplash.com/photo-1604011092346-0b4346ed714e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80'
+    ]);
 
     useFrame((state, delta) => {
         if (ref.current && zoomValue.current) {
@@ -47,31 +51,39 @@ function ShaderPlane() {
             ref.current.resolution = res;
             ref.current.zoom = zoomValue.current;
             ref.current.xy = xyValue.current;
+            // if (ref.current.uniforms.canvasTexture?.value) {
+            //     ref.current.uniforms.canvasTexture.value.needsUpdate = true;
+            // }
         }
     });
 
     return (
         <mesh scale={[width, height, 1]}>
             <planeGeometry />
-            <backgroundMaterial ref={ref} toneMapped={true} />
+            <mosaicMaterial ref={ref} toneMapped={true} uTexture={image} />
         </mesh>
     );
 }
 
 const Home: NextPage = () => {
     return (
-        <>
-            <main className=" top-0 h-screen w-screen ">
-                <Canvas className="absolute top-0 h-screen w-screen">
-                    <ShaderPlane />
-                </Canvas>
-                <div className="absolute top-0 z-10 h-full w-full text-white bg-blend-color-dodge">
-                    <div className="flex h-full w-full">
-                        <h1 className="mt-24 ml-10 mb-auto  text-9xl font-bold"></h1>
-                    </div>
+        <main className=" top-0 h-screen w-screen ">
+            <div className="line absolute top-32 right-32 h-10 w-20 mix-blend-color-dodge"></div>
+            <Canvas
+                dpr={[1, 2]}
+                className="e top-0  aspect-square min-h-max min-w-max"
+            >
+                <ShaderPlane />
+                <Stats showPanel={0} className="stats" />
+            </Canvas>
+            <div className="absolute top-0 z-10 h-screen w-full text-white bg-blend-color-dodge">
+                <div className="flex h-screen w-full">
+                    <h1 className="m-auto mb-auto  text-9xl font-bold italic">
+                        FINN DORE
+                    </h1>
                 </div>
-            </main>
-        </>
+            </div>
+        </main>
     );
 };
 
