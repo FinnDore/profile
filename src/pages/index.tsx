0d186546b97edc74ccaf913ffd500c948f2a8bca
Finn <34718806+FinnDore@users.noreply.github.com>
2022-12-31 19:46:00 +0000

    canvas also in a suspense

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,16 +10,17 @@ const ShaderPlane = lazy(() => import('../components/shader-plane.tsx'));
 const Home: NextPage = () => {
     return (
         <main className=" top-0 h-screen w-screen ">
-            <Canvas
-                dpr={[1, 2]}
-                className="e top-0  aspect-square min-h-max min-w-max"
-            >
-                <Suspense fallback={null}>
-                    <ShaderPlane />
-                </Suspense>
-                {/* <Stats showPanel={0} className="stats" /> */}
-            </Canvas>
-
+            <Suspense>
+                <Canvas
+                    dpr={[1, 2]}
+                    className="e top-0  aspect-square min-h-max min-w-max"
+                >
+                    <Suspense fallback={null}>
+                        <ShaderPlane />
+                    </Suspense>
+                    {/* <Stats showPanel={0} className="stats" /> */}
+                </Canvas>
+            </Suspense>
             <div className="absolute top-0 z-10 h-screen w-full text-white bg-blend-color-dodge">
                 <div className="flex h-screen w-full">
                     <h1 className="name m-auto mb-auto text-9xl font-bold italic">
