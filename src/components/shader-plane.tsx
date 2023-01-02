2b9f7248050b8d179aabb0162371a51d732c76c5
Finn <34718806+FinnDore@users.noreply.github.com>
2023-01-02 14:19:32 +0000

    more consistent

--- src/components/shader-plane.tsx
+++ src/components/shader-plane.tsx
@@ -1,27 +1,27 @@
 import { Stats } from '@react-three/drei';
 import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import type THREE from 'three';
-import { TextureLoader, Vector2 } from 'three';
+import { Clock, TextureLoader, Vector2 } from 'three';
 import '../components/mosaic-bg';
 import type { MosaicProps } from '../components/mosaic-bg';
 
 const ShaderPlane = () => {
     const ref = useRef<(THREE.ShaderMaterial & MosaicProps) | null>(null);
-    const zoomValue = useRef(1);
     const { width, height } = useThree(state => state.viewport);
     const res = new Vector2(width, width);
-
+    const [clock] = useState(new Clock());
     const [image] = useLoader(TextureLoader, ['/lines.png']);
     const passedImage = useRef(false);
 
     useFrame((state, delta) => {
-        const timeUntilNextFrame = 150 - delta;
+        const timeUntilNextFrame = 500 - clock.getDelta();
 
         setTimeout(() => {
             state.invalidate();
         }, Math.max(0, timeUntilNextFrame));
-        if (ref.current && zoomValue.current) {
+
+        if (ref.current) {
             ref.current.time += delta;
             ref.current.resolution = res;
             if (image && !passedImage.current) {
