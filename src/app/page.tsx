fab66c5e958b9bb47c0f1741747b6f05cb41e733
Finn <34718806+FinnDore@users.noreply.github.com>
2024-05-06 12:42:01 +0100

    small

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ export default function Page() {
     return (
         <main className="h-screen bg-white flex flex-col">
             <img
-                className="mx-auto  max-w-[75rem] relative aspect-auto"
+                className="m-auto md:hidden max-h-[70vh] relative aspect-auto"
+                src="/finn-sm.webp"
+                alt="Picture with the text 'finn'"
+            />
+            <img
+                className="mx-auto md:block hidden max-w-[clamp(80vw,75rem,90vw)] relative aspect-auto"
                 src="/finn.webp"
                 alt="Picture with the text 'finn'"
             />
