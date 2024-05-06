9ccbe4f68f1e79081917c0eeca727bfd214ac5ea
Finn <34718806+FinnDore@users.noreply.github.com>
2024-05-06 12:52:25 +0100

    format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 export default function Page() {
-    return (
-        <main className="h-screen bg-white flex flex-col">
-            <img
-                className="m-auto md:hidden max-h-[70vh] relative aspect-auto"
-                src="/finn-sm.webp"
-                alt="Picture with the text 'finn'"
-            />
-            <img
-                className="mx-auto md:block hidden max-w-[clamp(80vw,75rem,90vw)] relative aspect-auto"
-                src="/finn.webp"
-                alt="Picture with the text 'finn'"
-            />
-        </main>
-    );
+  return (
+    <main className="h-screen bg-white flex flex-col">
+      <img
+        className="m-auto md:hidden max-h-[70vh] relative aspect-auto"
+        src="/finn-sm.webp"
+        alt="Picture with the text 'finn'"
+      />
+      <img
+        className="mx-auto md:block hidden max-w-[clamp(80vw,75rem,90vw)] relative aspect-auto"
+        src="/finn.webp"
+        alt="Picture with the text 'finn'"
+      />
+    </main>
+  );
 }
