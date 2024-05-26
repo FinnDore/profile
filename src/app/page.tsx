35560b9f92126d57e4d6081b0b28bc373b3e684a
Finn <34718806+FinnDore@users.noreply.github.com>
2024-05-26 12:38:04 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TailwindIndicator } from "../components/tw-indicator";
 import { Projects } from "./(components)/projects";
 import { Spotify } from "./(components)/spotify";
 
@@ -9,42 +10,36 @@ const queryClient = new QueryClient();
 export default function Page() {
     return (
         <main className="flex h-screen flex-col bg-white">
+            <TailwindIndicator />
             <QueryClientProvider client={queryClient}>
                 <div className="relative z-10 m-auto w-screen md:w-auto md:pt-16">
                     <img
-                        className="relative m-auto aspect-auto max-h-[70vh] select-none contrast-125 md:hidden"
-                        src="/finn-sm.webp"
-                        alt="Picture with the text 'finn'"
-                    />
-                    <img
-                        className="relative mx-auto hidden aspect-auto max-w-[clamp(80vw,75rem,90vw)] select-none contrast-125 md:block"
+                        className="relative mx-auto mt-12 aspect-auto max-w-[clamp(80vw,75rem,90vw)] select-none contrast-125 md:mt-0"
                         src="/finn-crop.webp"
                         alt="Picture with the text 'finn'"
                     />
-                    <div className="mx-auto flex h-32 w-full justify-center px-4 md:max-w-[clamp(80vw,75rem,90vw)] md:-translate-y-16 md:justify-between xl:-translate-y-24">
+                    <div className="mx-auto mb-14 flex w-full justify-center px-4 md:mb-0 md:h-32 md:max-w-[clamp(80vw,75rem,90vw)] md:-translate-y-16 md:justify-between xl:h-24 xl:-translate-y-24">
                         <div className="relative hidden md:block">
                             <Spotify />
                         </div>
-                        <div className="my-auto flex flex-col gap-2 text-center md:text-left">
-                            <div className="flex gap-2">
-                                <h2 className="mb-auto italic opacity-80">
-                                    Ugh, i write code or something
-                                    {" - "}
-                                    <a
-                                        className="underline transition-all hover:text-rose-500"
-                                        href="https://github.com/finndore"
-                                    >
-                                        Github
-                                    </a>
-                                    {" - "}
-                                    <a
-                                        href="https://arc.net/folder/F8D3C5D7-7E2A-4FFC-912D-D6C03A0CFD5E"
-                                        className="underline transition-all hover:text-rose-500"
-                                    >
-                                        Projects
-                                    </a>
-                                </h2>
-                            </div>
+                        <div className="my-auto  flex  gap-2 text-center  md:text-left">
+                            <h2 className="mb-auto italic opacity-80">
+                                Ugh, i write code or something
+                                {" - "}
+                                <a
+                                    className="underline transition-all hover:text-rose-500"
+                                    href="https://github.com/finndore"
+                                >
+                                    Github
+                                </a>
+                                {" - "}
+                                <a
+                                    href="https://arc.net/folder/F8D3C5D7-7E2A-4FFC-912D-D6C03A0CFD5E"
+                                    className="underline transition-all hover:text-rose-500"
+                                >
+                                    Projects
+                                </a>
+                            </h2>
                         </div>
                     </div>
                 </div>
